5facb56fd35d651d2f097e8ac0d10110ccec9ac9
Pachj <Pachj@users.noreply.github.com>
2017-02-15 18:29:27 +0100

    weather icons added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by Henry on 15.02.17.
  */
+getLocation();
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (location) {
+            $("#test").html(location.coords.latitude + " " + location.coords.longitude);
+
+        })
+    }
+}
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=47&lon=8&appid=42d4c944c359e9102a517de5e8b362f7", function (data) {
+    console.log(data.name);
+});
