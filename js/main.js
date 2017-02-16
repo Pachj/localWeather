ffbf93c887e6612415536eab3ba82e3cb402fd2b
Pachj <Pachj@users.noreply.github.com>
2017-02-16 18:40:03 +0100

    all functionalitys added and some HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
 /**
  * Created by Henry on 15.02.17.
  */
-getLocation();
+var unit = "celsius";
+var temp = 0;
 
-function getLocation() {
-    $.getJSON("http://ip-api.com/json/?fields=country,city,lat,lon", function (location) {
+$(document).ready(function () {
+    getLocation();
 
-        var latitude = location.lat;
-        var longitude = location.lon;
 
-        getWeather(latitude, longitude);
+    function getLocation() {
+        $.getJSON("http://ip-api.com/json/?fields=country,city,lat,lon", function (location) {
+
+            var latitude = location.lat;
+            var longitude = location.lon;
+
+            getWeather(latitude, longitude);
+        });
+    }
+
+    function getWeather(latitude, longitude) {
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude
+            + "&appid=42d4c944c359e9102a517de5e8b362f7", function (data) {
+
+            displayWeather(data.name, data.sys.country, data.weather[0]);
+            displayTemp(data.main.temp, "celsius");
+            temp = data.main.temp;
+        });
+    }
+
+    function displayWeather(city, country, weather) {
+        $("#location").html(city + ", " + country);
+        $("#condition").html(weather.main);
+    }
+
+    function displayTemp(temp, desiredUnit) {
+        if (desiredUnit === "celsius") {
+            var celsius = Math.round(temp - 273.15);
+            $("#temp").html(celsius + " °C");
+        }
+
+        else if (desiredUnit === "fahrenheit") {
+            var fahrenheit = Math.round((temp * (9 / 5)) - 459.67);
+            $("#temp").html(fahrenheit + " °F");
+        }
+    }
+
+    $("#temp").click(function () {
+        if (unit === "celsius") {
+            unit = "fahrenheit";
+        }
+
+        else if (unit === "fahrenheit") {
+            unit = "celsius";
+        }
+
+        displayTemp(temp, unit);
     });
-}
 
-function getWeather(latitude, longitude) {
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude
-        + "&appid=42d4c944c359e9102a517de5e8b362f7", function (weather) {
-
-        console.log(weather);
-    });
-}
-
+});
