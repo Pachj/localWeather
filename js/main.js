33902892edafc08e3d98ef013d2258930d064b1c
Pachj <Pachj@users.noreply.github.com>
2017-02-16 14:57:47 +0100

    logic to get the location and the weather added

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@
 getLocation();
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (location) {
-            $("#test").html(location.coords.latitude + " " + location.coords.longitude);
+    $.getJSON("http://freegeoip.net/json/", function (location) {
 
-        })
-    }
+        var latitude = location.latitude;
+        var longitude = location.longitude;
+
+        getWeather(latitude, longitude);
+    });
 }
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=47&lon=8&appid=42d4c944c359e9102a517de5e8b362f7", function (data) {
-    console.log(data.name);
-});
+function getWeather(latitude, longitude) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude
+        + "&appid=42d4c944c359e9102a517de5e8b362f7", function (weather) {
+
+        console.log(weather);
+    });
+}
+
